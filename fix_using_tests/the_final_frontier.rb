86214254bf690e008395fb96cb144e5dc40cbfce
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-06 19:50:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,9 +12,7 @@ def crew
 end
 
 def greet_crew (crew)
-  new_crew = crew.each {|crew_member| 
-  "Hello #{crew_member}."
-  }
+  new_crew = crew.each {|crew_member| "Hello #{crew_member}."}
   new_crew
   binding.pry
 end
